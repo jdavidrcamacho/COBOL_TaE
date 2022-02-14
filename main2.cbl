f332ee9c078643292331f0af0ca6b15303b7cb97
jdavidrcamacho <joao.camacho@astro.up.pt>
2022-02-14 18:54:53 +0000

    HADAMARD

--- main2.cbl
+++ main2.cbl
@@ -9,16 +9,16 @@
        01 M1.
            02 A2 OCCURS 3 TIMES.
               03 A3 OCCURS 3 TIMES.
-                 04 ARRAY-A PIC S9(4).
+                 04 ARRAY-A PIC S9(4) VALUE 3.
        01 M2.
            02 B2 OCCURS 3 TIMES.
               03 B3 OCCURS 3 TIMES.
-                 04 ARRAY-B PIC S9(4).
+                 04 ARRAY-B PIC S9(4) VALUE 2.
        01 M12.
            02 C2 OCCURS 3 TIMES.
               03 C3 OCCURS 3 TIMES.
                  04 ARRAY-C PIC S9(1).
        PROCEDURE DIVISION.
-           DISPLAY M12.
+           DISPLAY M1.
+           DISPLAY M2.
            STOP RUN.
-
