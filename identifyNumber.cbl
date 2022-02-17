cd5960d06b36c0e694c3b4e85c45967b245aa316
jdavidrcamacho <jdavidrcamacho@gmail.com>
2022-02-17 15:23:04 +0000

    IDENTIFY NUMBER

---
+++ identifyNumber.cbl
@@ -0,0 +1,26 @@
+      * identify if a number is of a certain value
+      *****************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. identify. 
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+
+       WORKING-STORAGE SECTION.
+       01 VAL1 PIC S9(4)V9(4).
+       01 VAL2 PIC S9(4)V9(4).
+
+       PROCEDURE DIVISION.
+           DISPLAY "INSERT FIRST VALUE: "
+           ACCEPT VAL1.
+           DISPLAY "INSERT SECOND VALUE: "
+           ACCEPT VAL2.
+           IF (VAL1 > VAL2)
+              DISPLAY "THE FIRST VALUE IS BIGGER THAN " VAL2
+           ELSE
+              IF (VAL1 = VAL2)
+                 DISPLAY "THE NUMBERS ARE EQUAL "
+              ELSE
+                 DISPLAY  "THE FIRST IS SMALLER THAN " VAL2
+              END-IF
+           END-IF.
+           STOP RUN.
